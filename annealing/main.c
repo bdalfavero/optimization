2157afa0b4d8c35cba1c87e9e7bbef9eb807f91f
Benjamin DalFavero <bdalfavero@gmail.com>
2022-07-04 16:32:46 -0500

    prints sequence of sampled states.

--- annealing/main.c
+++ annealing/main.c
@@ -10,32 +10,47 @@
 
 int main() {
 
-    sales_problem *prob;
-    int* cycle;
+    tour tr;
+    int *cycle, *old_cycle;
+    int state_changed;
 
     srand((int)clock());
 
-    prob = make_random_instance(4, 10.0);
-    cycle = hamiltonian_cycle(prob->num_cities);
-
-    for (int i = 0; i < prob->num_cities; i++) {
-        for (int j = 0; j < prob->num_cities; j++) {
-            printf("%f ", (prob->distance_array)[prob->num_cities * i + j]);
+    tr = make_tour(10, 1.0, 1.0);
+    cycle = random_cycle(tr);
+    old_cycle = malloc(tr.num_cities * sizeof(int));
+    
+    for (int i = 0; i < tr.num_cities; i++) {
+        printf("%d ", cycle[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 10; i++) {
+        // make a copy of the current state for comparison
+        copy_cycle(tr, cycle, old_cycle);
+        // sample a new state and print
+        sample_cycle(tr, cycle, 10.);
+        for (int i = 0; i < tr.num_cities; i++) {
+            printf("%d ", cycle[i]);
+        }
+        // if the state did not change, print 'f'
+        // otherwise, print 't'
+        state_changed = 0;
+        for (int i = 0; i < tr.num_cities; i++) {
+            if (old_cycle[i] != cycle[i]) {
+                state_changed = 1;
+                break;
+            }
+        }
+        if (state_changed == 1) {
+            printf(" t");
+        } else {
+            printf(" f");
         }
         printf("\n");
     }
 
-    printf("\n");
-    for (int i = 0; i < prob->num_cities; i++) {
-        printf("%d ", cycle[i]);
-    }
-    printf("\n");
-
-    // evaluate the distance of a given cycle
-    printf("\ndistance: %f\n", cycle_distance(prob, cycle));
-
-    free(prob);
     free(cycle);
-
+    free(old_cycle);
+    
     return 0;
 }
